7b4329dfe441df1cff0b02cd9f0d57a97d1bd85a
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-02 13:30:36 -0500

    stash

--- app/router.js
+++ app/router.js
@@ -2,7 +2,10 @@
 const db = require('./connection')('mood_db','rootroot')
 
 function router( app ){
-    // app.get()
+    app.get('/api/notes', async ( req,res ) => {
+        const data = await db.query('SELECT * FROM notes')
+        res.send(data)
+    })
 
     app.post('/api/notes', async ( req, res ) => {
         await db.query('INSERT INTO notes (emotion, title, note) VALUES (?,?,?)', [req.body.emo, req.body.title, req.body.note])
