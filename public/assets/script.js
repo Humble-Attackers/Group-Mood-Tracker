d23021b68247370524b639883d78f3ff77258abc
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-04 11:09:21 -0500

    commiting

--- public/assets/script.js
+++ public/assets/script.js
@@ -7,7 +7,9 @@ let arr;
 function fetchJSON(url, method = "get", data = {}) {
   const fetchOptions = {
     method,
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+               "Session": localStorage.session || ''
+    }
   };
   if (method === "post" || method === "put")
     fetchOptions.body = JSON.stringify(data);
